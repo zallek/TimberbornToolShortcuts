d13a012820a1b21b491454ba79f2f406a79a4607
Nicolas Fortin <nicolas.fortin37@gmail.com>
2021-10-02 13:52:54 +0200

    Release 0.2.1

--- Plugin.cs
+++ Plugin.cs
@@ -8,7 +8,7 @@ using Timberborn.ToolSystem;
 
 namespace ToolShortcuts
 {
-    [BepInPlugin("com.zallek.Timberborn.ToolShortcuts", "Tool Shortcuts", "0.2.0")]
+    [BepInPlugin("com.zallek.Timberborn.ToolShortcuts", "Tool Shortcuts", "0.2.1")]
     public class Plugin : BaseUnityPlugin
     {
         internal static ManualLogSource Log;
