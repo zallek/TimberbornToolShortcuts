dc57f12958052127610b0276054ecc1192283478
Nicolas Fortin <nicolas.fortin37@gmail.com>
2021-10-02 13:47:03 +0200

    Display key binding on buttons

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Click on a tool group, then press key 1, 2, 3, ... to select the right tool.
 
-![TimberbornToolShortcuts](https://user-images.githubusercontent.com/1886834/135699055-f9f481fd-9eb4-42b9-8c0b-c21a92f724df.gif)
+![Demo](./demo.gif)
 
 Note: Time speed shortcuts have been reassigned to ALT-1, ALT-2, ALT-3
 
--- ToolGroupButonPatch.cs
+++ ToolGroupButonPatch.cs
@@ -1,11 +1,35 @@
 using HarmonyLib;
 using System.Collections.Generic;
 using Timberborn.ToolSystem;
+using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace ToolShortcuts
 {
     class ToolGroupButtonPatch
     {
+        [HarmonyPatch(typeof(ToolGroupButton), "PostLoad")]
+        public static class PatchPostLoad
+        {
+            private static void Postfix(List<ToolButton> ____toolButtons)
+            {
+                for (int i = 0; i < ____toolButtons.Count && i < 10; i++)
+                {
+                    string keyBinding = i == 9 ? "0" : $"{i + 1}";
+                    Label keyBindingLabel = new Label(keyBinding);
+                    keyBindingLabel.style.position = Position.Absolute;
+                    keyBindingLabel.style.top = 4;
+                    keyBindingLabel.style.left = 0;
+                    keyBindingLabel.style.width = 20;
+                    keyBindingLabel.style.backgroundColor = new Color(0f, 0f, 0f, 0f);
+                    keyBindingLabel.style.color = new Color(0.75f, 0.65f, 0.44f);
+                    keyBindingLabel.style.fontSize = 10;
+
+                    ____toolButtons[i].Root.Add(keyBindingLabel);
+                }
+            }
+        }
+
         [HarmonyPatch(typeof(ToolGroupButton), "OnToolGroupEntered")]
         public static class PatchOnToolGroupEntered
         {
